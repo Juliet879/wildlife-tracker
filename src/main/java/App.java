2fc03bdcebb8797729aa830a62f4f7c7d19da6ab
JulietK <julietgisemba000@>
2020-09-21 11:57:34 +0300

    added path

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import models.Animal;
+import models.EndangeredAnimal;
+import models.Sightings;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -10,6 +11,7 @@ import java.util.Map;
 
 public class App{
     public static void main(String[] args) {
+
         staticFileLocation("/public");
 
 
@@ -20,9 +22,32 @@ public class App{
 
         get("/wildlife/new",(request,response) ->{
             Map<String,Object> model = new HashMap<>();
-            return new ModelAndView(model,"wildlife.hbs")
-        })
+            return new ModelAndView(model,"wildlife.hbs");
+        }, new HandlebarsTemplateEngine());
 
+        post("/wildlife/new",(request,response) ->{
+            Map<String,Object> model = new HashMap<>();
+            int animalId = Integer.parseInt(request.queryParams("animalId"));
+            String location = request.queryParams("location");
+            String name = request.queryParams("name");
+            String ranger = request.queryParams("ranger");
+            String age = request.queryParams("age");
+            String health = request.queryParams("health");
+
+            if(age == null && health == null){
+                animal.save();
+            }
+            else {
+                EndangeredAnimal endangeredAnimal = new EndangeredAnimal(name);
+                endangeredAnimal.save();
+                endangeredAnimal.saveAge(age);
+                endangeredAnimal.saveHealth(health);
+            }
+            Sightings sightings = new Sightings(location,ranger,animalId);
+            sightings.save();
+            response.redirect("/all-animals");
+            return null;
+        }, new HandlebarsTemplateEngine());
     }
 
     private static void staticFileLocation(String s) {
