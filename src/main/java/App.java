70a349bd8d0cd8ff09b3abf9db31c8811ce53d38
JulietK <julietgisemba000@>
2020-09-21 10:40:02 +0300

    add routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,30 @@
-public class App{
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class App{
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/wildlife/new",(request,response) ->{
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model,"wildlife.hbs")
+        })
+
+    }
+
+    private static void staticFileLocation(String s) {
+    }
 }
