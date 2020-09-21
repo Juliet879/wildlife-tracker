46cdecf75d274d8827901e3704cf5fef5c6722ae
JulietK <julietgisemba000@>
2020-09-21 14:20:24 +0300

    add test

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,3 +1,7 @@
 public class Animal extends Wildlife{
-    public static final String DATABASE
+    public static final String DATABASE_TYPE = "animal";
+
+    public Animal(String name){
+
+    }
 }
---
+++ src/test/java/models/AnimalTest.java
@@ -0,0 +1,7 @@
+package models;
+
+import junit.framework.TestCase;
+
+public class AnimalTest extends TestCase {
+
+}
